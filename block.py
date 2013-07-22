5c0b8f1191cc5f9b9cf6848482737bd829240fef
Colby Chiang <cc2qe@server.fake>
2013-07-22 15:19:30 -0400

    block without loading full array

--- block.py
+++ block.py
@@ -39,30 +39,82 @@ description: Basic python script template")
 # primary function
 def block(file):
     prevGt = -1
+    obs = list()
+    exp = list()
     diffs = list()
+
+    ref1 = list()
+    alt1 = list()
+    ref2 = list()
+    alt2 = list()
+
+    # print header
+    print '\t'.join(("# gt", "mean", "stdev", "numPairs", "meanObs", "meanExp", "meanRef1", "meanRef2"))
     for line in file:
         v = line.rstrip().split('\t')
         gt = v[14]
 
-
         if gt != prevGt and prevGt != -1:
-            print "dump data %s" % prevGt
-            print "mean counts %s" % np.mean(diffs)
-            print "stdev %s" % np.std(diffs)
-            for i in range(len(diffs)):
-                if abs(diffs[i] - np.mean(diffs)):
-                    diffs[0:i] + diffs[i+1:len(diffs)]
+            for d in diffs:
+                if abs(d - np.mean(diffs) > 10):
+                    index = diffs.index(d)
+                    diffs.remove(d)
+                    obs.pop(index)
+                    exp.pop(index)
+                    ref1.pop(index)
+                    alt1.pop(index)
+                    ref2.pop(index)
+                    alt2.pop(index)
+            refMaj1 = list()
+            refMaj2 = list()
+            for i in range(len(ref1)):
+                refMaj1.append( ref1[i] - alt1[i] )
+                refMaj2.append( ref2[i] - alt2[i] )
+                    
                 # omitArr = diffs[0:i] + diffs[i+1:len(diffs)]
                 # print(np.std(omitArr))
+            print '%s\t%.2f\t%.2f\t%s\t%.2f\t%.2f\t%.2f\t%.2f' % (prevGt,np.mean(diffs),np.std(diffs),len(diffs),np.mean(obs),np.mean(exp),np.mean(refMaj1),np.mean(refMaj2))
+
             diffs = list()
+            obs = list()
+            exp = list()
+            ref1 = list()
+            alt1 = list()
+            ref2 = list()
+            alt2 = list()
         
-        print line.rstrip()
+        # print line.rstrip()
         counts = v[15].split('|')
         # difference between observed and expected
         diffs.append(float(counts[2]))
+        obs.append(float(counts[0]))
+        exp.append(float(counts[1]))
+
+        ref1.append(float(v[4]))
+        alt1.append(float(v[6]))
+        ref2.append(float(v[11]))
+        alt2.append(float(v[13]))
         
         prevGt = gt
     
+    for d in diffs:
+        if abs(d - np.mean(diffs) > 10):
+            index = diffs.index(d)
+            diffs.remove(d)
+            obs.pop(index)
+            exp.pop(index)
+            ref1.pop(index)
+            alt1.pop(index)
+            ref2.pop(index)
+            alt2.pop(index)
+            
+        for i in range(len(ref1)):
+            refMaj1.append( ref1[i] - alt1[i] )
+            refMaj2.append( ref2[i] - alt2[i] )
+
+            # omitArr = diffs[0:i] + diffs[i+1:len(diffs)]
+            # print(np.std(omitArr))
+    print '%s\t%.2f\t%.2f\t%s\t%.2f\t%.2f\t%.2f\t%.2f' % (prevGt,np.mean(diffs),np.std(diffs),len(diffs),np.mean(obs),np.mean(exp),np.mean(refMaj1),np.mean(refMaj2))
     return
 
 # --------------------------------------
