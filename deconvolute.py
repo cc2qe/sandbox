a6a68a9017d25f82c3d1978358a389a3f2606382
Colby Chiang <cc2qe@virginia.edu>
2013-10-08 13:25:06 -0400

    added hybrid calling of variants

--- deconvolute.py
+++ deconvolute.py
@@ -50,95 +50,59 @@ def decon(method, seg_file):
         (log_copy, min_frac) = map(float, (log_copy, min_frac))
         copy_ratio = 2 ** log_copy
 
-        # let R be the list of aberrant copy number ratios. These
-        # fractions are rational numbers based on the assumption of
-        # integer number of chromosomes
-        R = [0.5, 3/2.0, 4/2.0]
+        if method == 'copy':
+            # let R be the list of aberrant copy number ratios. These
+            # fractions are rational numbers based on the assumption of
+            # integer number of chromosomes
+            R = [0.5, 3/2.0, 4/2.0]
+
+            # let S be the list of aberrant minor allele fractions. These
+            # fractions are rational numbers based on the assumption of
+            # integer number of chromosomes
+            S = [0, 1/3.0, 1/4.0]
+
+            # just for the output string, to convert y_best to number of copies
+            copy_count = [1,3,4]
+
+            # make blank matrices for the residuals
+            x_copy_resids = [0]*len(R)
+            x_min_frac_resids = [0]*len(R)
+            x_sols = [None]*len(R)
+            
+            # iterate over each aberrant possibility
+            for i in range(len(R)):
+                r = R[i]
+                s = S[i]
+
+                # Solve based on copy number first (alpha is coeff for copy number, beta is
+                # coeff for minor allele fraction
+                alpha = 1
+                beta = 0
+
+                # A * x = B, solve for x.
+                A = np.matrix([[alpha*1 + beta*0.5 ,alpha*r + beta*s],[1,1]])
+                B = np.matrix([[alpha*copy_ratio + beta*min_frac],[1]])
+                x = np.linalg.solve(A,B)
+
+                # if any of the subpops are > 1 or < 0, then peg them to 1 and 0
+                for m in range(x.shape[0]):
+                    for n in range(x.shape[1]):
+                        if x[m,n] < 0:
+                            x[m,n] = 0
+                        elif x[m,n] > 1:
+                            x[m,n] = 1
         
-        # let S be the list of aberrant minor allele fractions. These
-        # fractions are rational numbers based on the assumption of
-        # integer number of chromosomes
-        S = [0, 1/3.0, 1/4.0]
-    
-        # make blank matrices for the residuals
-        x_copy_resids = [0]*len(R)
-        y_copy_resids = [0]*len(R)
-        x_min_frac_resids = [0]*len(R)
-        y_min_frac_resids = [0]*len(R)
-        x_sols = [None]*len(R)
-        y_sols = [None]*len(R)
+                # Now we have copy number solutions (x) and minor allele fraction solutions (y)
+                # Calculate the residuals off the other metric for each
+                x_copy_ratio = x[0,0]*1 + x[1,0]*r
+                x_min_frac = x[0,0]*0.5 + x[1,0]*s
+                x_copy_resids[i] = abs(x_copy_ratio - copy_ratio)    # this should always be zero
+                x_min_frac_resids[i] = abs(x_min_frac - min_frac)
+                x_sols[i] = x
+        
+            # now we have the copy number and allele solutions. determine the best one
+            x_best = x_min_frac_resids.index(min(x_min_frac_resids))
 
-        # iterate over each aberrant possibility
-        for i in range(len(R)):
-            r = R[i]
-            s = S[i]
-
-            # Solve based on copy number first (alpha is coeff for copy number, beta is
-            # coeff for minor allele fraction
-            alpha = 1
-            beta = 0
-
-            # A * x = B, solve for x.
-            A = np.matrix([[alpha*1 + beta*0.5 ,alpha*r + beta*s],[1,1]])
-            B = np.matrix([[alpha*copy_ratio + beta*min_frac],[1]])
-            x = np.linalg.solve(A,B)
-            
-            # if any of the subpops are > 1 or < 0, then peg them to 1 and 0
-            for m in range(x.shape[0]):
-                for n in range(x.shape[1]):
-                    if x[m,n] < 0:
-                        x[m,n] = 0
-                    elif x[m,n] > 1:
-                        x[m,n] = 1
-
-            # Solve based on minor allele fraction
-            alpha = 0
-            beta = 1
-
-            # A * y = B, solve for y.
-            A = np.matrix([[alpha*1 + beta*0.5 ,alpha*r + beta*s],[1,1]])
-            B = np.matrix([[alpha*copy_ratio + beta*min_frac],[1]])
-            y = np.linalg.solve(A,B)
-
-            # if any of the subpops are > 1 or < 0, then peg them to 1 and 0
-            for m in range(y.shape[0]):
-                for n in range(y.shape[1]):
-                    if y[m,n] < 0:
-                        y[m,n] = 0
-                    elif y[m,n] > 1:
-                        y[m,n] = 1
-
-            # Now we have copy number solutions (x) and minor allele fraction solutions (y)
-            # Calculate the residuals off the other metric for each
-            x_copy_ratio = x[0,0]*1 + x[1,0]*r
-            x_min_frac = x[0,0]*0.5 + x[1,0]*s
-            x_copy_resids[i] = abs(x_copy_ratio - copy_ratio)    # this should always be zero
-            x_min_frac_resids[i] = abs(x_min_frac - min_frac)
-            x_sols[i] = x
-
-            # do the same for the minor allele fraction solutions
-            y_copy_ratio = y[0,0]*1 + y[1,0]*r
-            y_min_frac = y[0,0]*0.5 + y[1,0]*s
-            y_copy_resids[i] = abs(y_copy_ratio - copy_ratio)
-            y_min_frac_resids[i] = abs(y_min_frac - min_frac)    # this should always be zero
-            y_sols[i] = y
-            
-        # now we have the copy number and allele solutions. determine the best one
-        x_best = x_min_frac_resids.index(min(x_min_frac_resids))
-        y_best = y_copy_resids.index(min(y_copy_resids))
-
-        # just for the output string, to convert y_best to number of copies
-        copy_count = [1,3,4]
-
-        if method == 'allele':
-            # the best calculations for the allele method
-            y = y_sols[y_best]
-            s = S[y_best]
-            r = R[y_best]
-            y_copy_ratio = y[0,0]*1 + y[1,0]*r
-            y_min_frac = y[0,0]*0.5 + y[1,0]*s
-            print '\t'.join(map(str, (chrom, start, end, seg_id, copy_count[y_best], r, "%.3f" % s, "%.3f" % y[0,0], "%.3f" % y[1,0], "%.3f" % copy_ratio, "%.3f" % y_copy_ratio, "%.3f" % min_frac, "%.3f" % y_min_frac)))
-        elif method == 'copy':
             # the best calculations for the copy number method
             x = x_sols[x_best]
             s = S[x_best]
@@ -146,6 +110,129 @@ def decon(method, seg_file):
             x_copy_ratio = x[0,0]*1 + x[1,0]*r
             x_min_frac = x[0,0]*0.5 + x[1,0]*s
             print '\t'.join(map(str, (chrom, start, end, seg_id, copy_count[x_best], r, "%.3f" % s, "%.3f" % x[0,0], "%.3f" % x[1,0], "%.3f" % copy_ratio, "%.3f" % x_copy_ratio, "%.3f" % min_frac, "%.3f" % x_min_frac)))
+            
+        elif method == 'allele':
+            # let R be the list of aberrant copy number ratios. These
+            # fractions are rational numbers based on the assumption of
+            # integer number of chromosomes
+            R = [0.5, 3/2.0, 4/2.0]
+
+            # let S be the list of aberrant minor allele fractions. These
+            # fractions are rational numbers based on the assumption of
+            # integer number of chromosomes
+            S = [0, 1/3.0, 1/4.0]
+
+            # just for the output string, to convert y_best to number of copies
+            copy_count = [1,3,4]
+
+            # make blank matrices for the residuals
+            y_copy_resids = [0]*len(R)
+            y_min_frac_resids = [0]*len(R)
+            y_sols = [None]*len(R)
+            
+            # iterate over each aberrant possibility
+            for i in range(len(R)):
+                r = R[i]
+                s = S[i]
+
+                # Solve based on minor allele fraction
+                alpha = 0
+                beta = 1
+
+                # A * y = B, solve for y.
+                A = np.matrix([[alpha*1 + beta*0.5 ,alpha*r + beta*s],[1,1]])
+                B = np.matrix([[alpha*copy_ratio + beta*min_frac],[1]])
+                y = np.linalg.solve(A,B)
+
+                # if any of the subpops are > 1 or < 0, then peg them to 1 and 0
+                for m in range(y.shape[0]):
+                    for n in range(y.shape[1]):
+                        if y[m,n] < 0:
+                            y[m,n] = 0
+                        elif y[m,n] > 1:
+                            y[m,n] = 1
+                
+                # Now we have copy number solutions (x) and minor allele fraction solutions (y)
+                # Calculate the residuals off the other metric for each
+                y_copy_ratio = y[0,0]*1 + y[1,0]*r
+                y_min_frac = y[0,0]*0.5 + y[1,0]*s
+                y_copy_resids[i] = abs(y_copy_ratio - copy_ratio)
+                y_min_frac_resids[i] = abs(y_min_frac - min_frac)    # this should always be zero
+                y_sols[i] = y
+            
+            # now we have the copy number and allele solutions. determine the best one
+            y_best = y_copy_resids.index(min(y_copy_resids))
+            
+            # the best calculations for the allele method
+            y = y_sols[y_best]
+            s = S[y_best]
+            r = R[y_best]
+            y_copy_ratio = y[0,0]*1 + y[1,0]*r
+            y_min_frac = y[0,0]*0.5 + y[1,0]*s
+            print '\t'.join(map(str, (chrom, start, end, seg_id, copy_count[y_best], r, "%.3f" % s, "%.3f" % y[0,0], "%.3f" % y[1,0], "%.3f" % copy_ratio, "%.3f" % y_copy_ratio, "%.3f" % min_frac, "%.3f" % y_min_frac)))
+        
+        elif method == 'hybrid':
+            # let R be the list of aberrant copy number ratios. These
+            # fractions are rational numbers based on the assumption of
+            # integer number of chromosomes
+            R = [0.5, 1, 3/2.0, 4/2.0, 4/2.0, 5/2.0, 5/2.0]
+
+            # let S be the list of aberrant minor allele fractions. These
+            # fractions are rational numbers based on the assumption of
+            # integer number of chromosomes
+            S = [0, 0, 1/3.0, 1/4.0, 1/2.0, 1/5.0, 2/5.0]
+
+            # just for the output string, to convert y_best to number of copies
+            copy_count = [1,2,3,4,4,5,5]
+
+            # make blank matrices for the residuals
+            z_copy_resids = [0]*len(R)
+            z_min_frac_resids = [0]*len(R)
+            z_combo_resids = [0]*len(R)
+            z_sols = [None]*len(R)
+
+            # iterate over each aberrant possibility
+            for i in range(len(R)):
+                r = R[i]
+                s = S[i]
+                # Solve based on combo
+                alpha = 1
+                beta = 1
+                
+                # A * y = B, solve for y.
+                A = np.matrix([[alpha*1 + beta*0.5 ,alpha*r + beta*s],[1,1]])
+                B = np.matrix([[alpha*copy_ratio + beta*min_frac],[1]])
+                z = np.linalg.solve(A,B)
+                
+                # if any of the subpops are > 1 or < 0, then peg them to 1 and 0
+                for m in range(z.shape[0]):
+                    for n in range(z.shape[1]):
+                        if z[m,n] < 0:
+                            z[m,n] = 0
+                        elif z[m,n] > 1:
+                            z[m,n] = 1
+
+                # Now we have copy number solutions and minor allele fraction solutions
+                # Calculate the residuals off the other metric for each
+                z_copy_ratio = z[0,0]*1 + z[1,0]*r
+                z_min_frac = z[0,0]*0.5 + z[1,0]*s
+                z_copy_resids[i] = abs(z_copy_ratio - copy_ratio)
+                z_min_frac_resids[i] = abs(z_min_frac - min_frac)
+                z_combo_resids[i] = z_copy_resids[i] + z_min_frac_resids[i]
+                z_sols[i] = z
+
+                # print '\t'.join(map(str, (chrom, start, end, seg_id, copy_count[i], r, "%.3f" % s, "%.3f" % z[0,0], "%.3f" % z[1,0], "%.3f" % copy_ratio, "%.3f" % z_copy_ratio, "%.3f" % min_frac, "%.3f" % z_min_frac)))
+
+            # now we have the copy number and allele solutions. determine the best one
+            z_best = z_combo_resids.index(min(z_combo_resids))
+
+            # the best calculations for the allele method
+            z = z_sols[z_best]
+            s = S[z_best]
+            r = R[z_best]
+            z_copy_ratio = z[0,0]*1 + z[1,0]*r
+            z_min_frac = z[0,0]*0.5 + z[1,0]*s
+            print '\t'.join(map(str, (chrom, start, end, seg_id, copy_count[z_best], r, "%.3f" % s, "%.3f" % z[0,0], "%.3f" % z[1,0], "%.3f" % copy_ratio, "%.3f" % z_copy_ratio, "%.3f" % min_frac, "%.3f" % z_min_frac)))      
     
     return
 
