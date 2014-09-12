1a85132a3e69c1aae77de4e505a8dd0e7c8a169c
Colby Chiang <cc2qe@virginia.edu>
2014-09-11 19:59:44 -0500

    works on -s 11 -n 30 start_temp 200, alpha 0.9999, maxevals 100000

--- simanneal.py
+++ simanneal.py
@@ -60,47 +60,49 @@ def total_dist(points, route):
         a = b
     return d
     
-def binary_flip(route):
+def binary_flip(route, a, b):
     new_route = route[:]
-    a,b = random.sample(xrange(1, len(route)-2),2)
     new_route[a], new_route[b] = route[b], route[a]
     return new_route
 
 # decide if to switch
 def switch(dist, new_dist, temp):
     if new_dist < dist:
-        return True
+        p = 1.0
     else:
-        scale = math.exp(-abs(new_dist - dist)/temp)
-        x = random.random() * scale
-        print scale, x, temp
-        if x < temp:
-            return True
-        else:
-            return False
+        p = math.exp(-abs(new_dist - dist)/temp)
 
-def temp_fx(T, alpha):
-    return T * alpha
+    print temp, dist, new_dist, p
+    if random.random() < p:
+            return True
+    else:
+        return False
+
+def kirkpatrick_cooling(start_temp, alpha):
+    T = start_temp
+    while True:
+        yield T
+        T = alpha * T
 
 # primary function
 def myFunction(size):
-
-    # total_epochs = 100000
-    start_temp = 10000
-    alpha = 0.999
-    max_evals = 1000000
+    start_temp = 200
+    alpha = 0.9999
+    max_evals = 100000
 
     points = []
     for i in xrange(size):
         coord = [random.random(), random.random()]
         points.append(coord)
 
+    # print points
     # print points[5], points[9]
     # print dist(points[5], points[9])
 
     route = range(size)
     random.shuffle(route)
     route.append(route[0])
+    print route
     d = total_dist(points, route)
 
     print 'initial dist', d
@@ -109,29 +111,51 @@ def myFunction(size):
     min_dist = d
 
     i = 0
-    temp = temp_fx(start_temp, alpha)
-    # while i < max_evals:
-    while temp > 1:
-        flip_route = binary_flip(route)
-        flip_d = total_dist(points, flip_route)
 
-        if switch(d, flip_d, temp):
-            route = flip_route
-            d = flip_d
+    cooling_schedule = kirkpatrick_cooling(start_temp, alpha)
+    path_exhaust = 0 # number of temps that have iterated because of exhausted paths
+    for temp in cooling_schedule:
+        done = False
+        tried = set()
+        while not done:
+            while 1:
+                a,b = random.sample(xrange(1, len(route)-1),2)
+                if (a,b) not in tried: break
+            flip_route = binary_flip(route, a, b)
+            flip_d = total_dist(points, flip_route)
+            tried.add((a,b))
 
-        if d < min_dist:
-            min_dist = d
-            best_route = route[:]
+            print i
+            print len(tried)
+            if switch(d, flip_d, temp):
+                route = flip_route
+                d = flip_d
 
-        if i % 1000 == 0:
+                # only iterate temp if accept
+                i += 1
+                done = True
+
+            if d < min_dist:
+                min_dist = d
+                best_route = route[:]
+
+            if len(tried) >= (size - 2) * (size - 1):
+                i += 1
+                path_exhaust += 1
+                done = True
+
+        if i % max_evals/50 == 0:
             f = open('routes/route_%s.txt' % i, 'w')
             for r in route:
                 f.write('\t'.join(map(str, points[r])) + '\n')
-
             f.close()
+        if path_exhaust > 5 or i >= max_evals:
+            break
 
-        temp = temp_fx(temp, alpha)
-        i += 1
+    f = open('routes/route_best.txt', 'w')
+    for r in best_route:
+        f.write('\t'.join(map(str, points[r])) + '\n')
+    f.close()
 
     print best_route
     print min_dist
