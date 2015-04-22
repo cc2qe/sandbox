d9ad45870c881cdd115f73cb5a85e49d9fcb5373
Colby Chiang <cc2qe@virginia.edu>
2015-04-21 23:15:27 -0500

    works

--- upperquartilenorm.py
+++ upperquartilenorm.py
@@ -19,8 +19,8 @@ author: " + __author__ + "\n\
 description: upper quartile normalize expression data")
     # parser.add_argument('-a', '--abs', action='store_true', help='take absolute values of input before calculating stats')
     parser.add_argument('-q', '--quantile', type=str, default=0.75, help='quantile for normalization [0.75]')
-    parser.add_argument('-sr', '--skip_rows', type=int, default=0, help='number of rows to skip [0]')
-    parser.add_argument('-sc', '--skip_columns', type=int, default=0, help='number of columns to skip [0]')
+    parser.add_argument('-r', '--skip_rows', type=int, default=0, help='number of rows to skip [0]')
+    parser.add_argument('-c', '--skip_columns', type=int, default=0, help='number of columns to skip [0]')
     parser.add_argument('data', nargs='?', type=argparse.FileType('r'), default=None, help='input data [stdin]')
 
     # parse the arguments
@@ -36,28 +36,17 @@ description: upper quartile normalize expression data")
     return args
 
 # primary function
-def upperquartilenorm(data, quantile):
-    py_array = list() # temporary python structure
-    # d = np.array()
-    # for line in data:
-    #     # d.append(line.rstrip().split('\t'))
-    #     py_array.append(line.rstrip().split('\t'))
-    # print [p[4:] for p in py_array[1:]]
-    skip_rows = 1
-    skip_cols = 4
-
+def upperquartilenorm(data,
+                      quantile,
+                      skip_rows,
+                      skip_columns):
+    # store the row and column headers so we can
+    # output them at the end
     d_str = np.genfromtxt(data, dtype=str, comments=None)
     row_head = d_str[:skip_rows,:]
-    col_head = d_str[:,:skip_cols]
+    col_head = d_str[:,:skip_columns]
 
-    d = d_str[skip_rows:,skip_cols:].astype(float)
-
-    print row_head
-    print col_head
-    # d = np.genfromtxt(data, skiprows=1)[:,4:]
-    # d = np.array([p[4:] for p in py_array[1:]])
-    print d
-
+    d = d_str[skip_rows:,skip_columns:].astype(float)
     num_rows = d.shape[0]
     num_cols = d.shape[1]
 
@@ -71,13 +60,15 @@ def upperquartilenorm(data, quantile):
     # the mean of the quantile values
     mean_quantile_val = sum(quantile_val) / num_cols
 
-    # populate the normalized output array
-    d_norm = np.zeros_like(d)
+    # print the header
+    for i in xrange(row_head.shape[0]):
+        print '\t'.join(row_head[i,:])
+
+    # print the name columns plus the actual data
     for i in xrange(num_rows):
-        d_norm[i,:] = d[i,:] / quantile_val
-
-    np.savetxt(sys.stdout, d_norm, fmt='%.6f', delimiter='\t')
-
+        print '\t'.join(col_head[i + skip_rows,:].tolist() +
+                        ["%0.6f" % x for x in d[i,:] / quantile_val]
+                        )
     return
 
 # --------------------------------------
@@ -87,15 +78,14 @@ def main():
     # parse the command line args
     args = get_args()
 
-    # store into global values
-    data = args.data
-    quantile = args.quantile
-    
     # call primary function
-    upperquartilenorm(data, quantile)
+    upperquartilenorm(args.data,
+                      args.quantile,
+                      args.skip_rows,
+                      args.skip_columns)
     
     # close the file
-    data.close()
+    args.data.close()
 
 # initialize the script
 if __name__ == '__main__':
