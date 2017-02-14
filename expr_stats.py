aa16f1edae597d65d58eff154b0e88cf7096733f
Colby Chiang <cc2qe@virginia.edu>
2017-02-14 15:09:52 -0600

    update

---
+++ expr_stats.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import argparse, sys
+from argparse import RawTextHelpFormatter
+
+__author__ = "Author (cchiang@genome.wustl.edu)"
+__version__ = "$Revision: 0.0.1 $"
+__date__ = "$Date: 2015-07-14 14:31 $"
+
+# --------------------------------------
+# define functions
+
+def get_args():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter, description="\
+expr_stats.py\n\
+author: " + __author__ + "\n\
+version: " + __version__ + "\n\
+description: Basic python script template")
+    parser.add_argument('-sr', '--argA', metavar='argA', type=str, required=True, help='description of argument')
+    parser.add_argument('-b', '--argB', metavar='argB', required=False, help='description of argument B')
+    parser.add_argument('-c', '--flagC', required=False, action='store_true', help='sets flagC to true')
+    parser.add_argument('input', nargs='?', type=argparse.FileType('r'), default=None, help='file to read. If \'-\' or absent then defaults to stdin.')
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    # if no input, check if part of pipe and if so, read stdin.
+    if args.input == None:
+        if sys.stdin.isatty():
+            parser.print_help()
+            exit(1)
+        else:
+            args.input = sys.stdin
+
+    # send back the user input
+    return args
+
+# primary function
+def myFunction(argA, argB, flagC, file):
+    for line in file:
+        print line.rstrip()
+    
+    return
+
+# --------------------------------------
+# main function
+
+def main():
+    # parse the command line args
+    args = get_args()
+
+    # call primary function
+    myFunction(args.argA, args.argB, args.flagC, args.input)
+
+    # close the input file
+    args.input.close()
+
+# initialize the script
+if __name__ == '__main__':
+    try:
+        sys.exit(main())
+    except IOError, e:
+        if e.errno != 32:  # ignore SIGPIPE
+            raise
